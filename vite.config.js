ccbe4a6ac439a3594ccfd749cc5b5598beb48efa
TOSHKA-711 <aliovich711@gmail.com>
2024-09-16 22:48:11 +0300

    first

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Set the base URL for your project
+  base: '/e-commerce/',  // Replace with your actual subdirectory or repo name
 })
